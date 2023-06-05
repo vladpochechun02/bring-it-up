7088ed0f6fc6bc92cd9d6ae414454e394b9697bc
Vladyslav <kpigoodvlad@gmail.com>
2023-06-05 22:23:40 +0300

    Создал общий прототип слайдера и главные слайдеры

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -1,10 +1,8 @@
-export default class Slider {
+import Slider from "./slider";
+
+export default class MainSlider extends Slider {
     constructor(page, btns){
-        this.page = document.querySelector(page);
-        // this.slides = this.page.children;
-        this.slides = Array.from(this.page.children);
-        this.btns = document.querySelectorAll(btns);
-        this.slideIndex = 1;
+        super(page, btns);
     }
 
     showSlides(n) {
@@ -61,4 +59,4 @@ export default class Slider {
 
         this.showSlides(this.slideIndex);
     }
-}
+}   

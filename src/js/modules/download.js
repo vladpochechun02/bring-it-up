b55116b364ff29b9d27a8b03ce4144ae5ba57d89
Vladyslav <kpigoodvlad@gmail.com>
2023-06-08 22:56:24 +0300

    Реализовал функционал разворачивания контента и загрузки файлов

---
+++ src/js/modules/download.js
@@ -0,0 +1,31 @@
+export default class Download {
+    constructor(triggers) {
+        this.btns = document.querySelectorAll(triggers);
+        this.path = 'assets/img/mainbg.jpg';
+    }
+
+    downloadItem(path) {
+        const link = document.createElement('a');
+        
+        link.setAttribute('href', path);
+
+        link.setAttribute('download', 'nice_picture');
+
+        link.style.display = 'none';
+        document.body.appendChild(link);
+
+        link.click();
+
+    document.body.removeChild(link);
+    }
+
+    init() {
+        this.btns.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                this.downloadItem(this.path);
+            });
+        });
+    }
+}
